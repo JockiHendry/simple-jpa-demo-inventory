64fb60166475f9cc69fea028e383fced4b742180
Jocki Hendry <jocki.hendry@gmail.com>
2014-06-14 19:43:10 +0700

    Add `BONUS_POINT_RATE` to `KeyPengaturan`.

--- src/main/domain/pengaturan/KeyPengaturan.java
+++ src/main/domain/pengaturan/KeyPengaturan.java
@@ -19,7 +19,8 @@ public enum KeyPengaturan {
 
     SUPERVISOR_PASSWORD("Supervisor Password", JenisNilai.PASSWORD, new byte[] {-126, 124, -53, 14, -22, -118, 112, 108, 76, 52, -95, 104, -111, -8, 78, 123}),
     MASA_JATUH_TEMPO("Jatuh Tempo (Hari)", JenisNilai.INTEGER, 30),
-    UKURAN_HURUF_TABEL("Ukuran Huruf Tabel", JenisNilai.INTEGER, 0)
+    UKURAN_HURUF_TABEL("Ukuran Huruf Tabel", JenisNilai.INTEGER, 0),
+    BONUS_POINT_RATE("Bonus Point Rate", JenisNilai.INTEGER, 2000)
     ;
 
     public String description;
