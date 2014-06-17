87012b566ac3d141e52865c16b6736643d47c335
Jocki Hendry <jocki.hendry@gmail.com>
2014-06-17 17:30:18 +0700

    Disable add new product in inventory adjustment.

--- griffon-app/controllers/project/inventory/PenyesuaianStokController.groovy
+++ griffon-app/controllers/project/inventory/PenyesuaianStokController.groovy
@@ -116,7 +116,7 @@ class PenyesuaianStokController {
 
     def showItemBarang = {
         execInsideUISync {
-            def args = [parent: view.table.selectionModel.selected[0], listItemBarang: model.items]
+            def args = [parent: view.table.selectionModel.selected[0], listItemBarang: model.items, allowTambahProduk: false]
             def dialogProps = [title: 'Detail Item', size: new Dimension(900, 420)]
             DialogUtils.showMVCGroup('itemBarangAsChild', args, app, view, dialogProps) { m, v, c ->
                 model.items.clear()
