1f64c5e84d1f746c5eb716b74bf75188d831d1ea
Jocki Hendry <jocki.hendry@gmail.com>
2014-07-29 20:30:07 +0700

    Set version to 0.3.

--- griffon-app/conf/BuildConfig.groovy
+++ griffon-app/conf/BuildConfig.groovy
@@ -187,7 +187,7 @@ griffon.project.dependency.resolution = {
     }
     dependencies {
         runtime 'mysql:mysql-connector-java:5.1.20'
-        runtime 'org.hibernate:hibernate-entitymanager:4.3.4.Final'
+        runtime 'jockihendry:hibernate-entitymanager:4.3.7-EXPERIMENT'
         compile 'org.hibernate:hibernate-validator:4.3.0.Final'
         compile('net.sf.jasperreports:jasperreports:5.5.1') {
             exclude 'commons-collections'
